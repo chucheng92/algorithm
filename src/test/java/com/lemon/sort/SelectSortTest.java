9d680d4f60887d2b713a4afdb7c57c862fedf6f0
、Minami Kotori <932191671@qq.com>
2016-07-18 09:32:39 +0800

    update

--- src/test/java/com/lemon/sort/SelectSortTest.java
+++ src/test/java/com/lemon/sort/SelectSortTest.java
@@ -5,13 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SelectSortTest {
+
 	@Test
 	public void testSelectSort() {
 		int[] a = {28, 16, 32, 12, 60, 2, 5, 72};
 		new SelectSort().selectSort(a);
-		System.out.println("======选择排序测试======");
-		for (int i = 0; i < a.length; ++i) {
-			System.out.print(a[i] + " ");
-		}
+        assertArrayEquals(a, new int[]{2,5,12,16,28,32,60,72});
 	}
 }
