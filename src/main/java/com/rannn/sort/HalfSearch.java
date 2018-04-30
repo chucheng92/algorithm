408c0ff3fa13f47ce52f6f712a906374c65497bc
lemonjing <932191671@qq.com>
2018-04-30 12:34:25 +0800

    add scala heap sort

--- src/main/java/com/rannn/sort/HalfSearch.java
+++ src/main/java/com/rannn/sort/HalfSearch.java
@@ -1,7 +1,5 @@
 package com.rannn.sort;
 
-import java.util.Arrays;
-
 /**
  * Created by Lemonjing on 2015/4/18.
  * Github: Lemonjing
@@ -19,11 +17,5 @@ public class HalfSearch {
                 return mid;
         }
         return -1;
-    }
-
-    public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println(Arrays.binarySearch(a, 5));
-        System.out.println(new HalfSearch().halfSearch(a, 0, a.length - 1, 5));
     }
 }
